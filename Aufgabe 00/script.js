bb06ccf8089431b92ce5b1b5d1d2f82bb42ebdeb
helenareimold <48830859+helenareimold@users.noreply.github.com>
2020-10-08 15:02:20 +0200

    .

---
+++ Aufgabe 00/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    let yourname = prompt("Sag mir deinen Namen!", "Hier bitte eintragen..");
+    document.getElementById("Hier ist dein Name: ").innerHTML = "Hier ist dein Name: " + yourname;
+};
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe 00/script.ts
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let yourname = prompt("Sag mir deinen Namen!", "Hier bitte eintragen..");
+    
+        document.getElementById("Hier ist dein Name: ").innerHTML = "Hier ist dein Name: " + yourname;
+    
+}
