bc14ff91f9f4422541ba5596c8307718c3a66809
helenareimold <48830859+helenareimold@users.noreply.github.com>
2021-02-05 17:59:41 +0100

    .

--- ENDABGABE/Server/server.js
+++ ENDABGABE/Server/server.js
@@ -1,16 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = require("http");
 const Url = require("url");
+const Mongo = require("mongodb");
 var ENDABGABE_EIA2;
 (function (ENDABGABE_EIA2) {
-    let server = Http.createServer();
-    let port = process.env.PORT;
-    if (port == undefined)
-        port = 5001;
-    console.log("Server starting on port:" + port);
-    server.listen(port);
-    server.addListener("request", handleRequest);
+    let rocket;
+    let databaseUrl = "mongodb+srv://helenareimold:fireworkseditor@cluster1.dyyg0.mongodb.net/FireworksEditor?retryWrites=true&w=majority";
+    startServer();
+    connectToDatabase(databaseUrl);
+    function startServer() {
+        let server = Http.createServer();
+        let port = process.env.PORT;
+        if (port == undefined)
+            port = 5001;
+        console.log("Server starting on port:" + port);
+        server.listen(port);
+        server.addListener("request", handleRequest);
+    }
     function handleRequest(_request, _response) {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
@@ -20,6 +35,14 @@ var ENDABGABE_EIA2;
             _response.write(jsonString);
         }
         _response.end();
+    }
+    function connectToDatabase(_url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let options = { useNewUrlParser: true, useUnifiedTopology: true };
+            let mongoClient = new Mongo.MongoClient(_url, options);
+            yield mongoClient.connect();
+            rocket = mongoClient.db("FireworksEditor").collection("Rockets");
+        });
     }
 })(ENDABGABE_EIA2 = exports.ENDABGABE_EIA2 || (exports.ENDABGABE_EIA2 = {}));
 //# sourceMappingURL=server.js.map
--- ENDABGABE/Server/server.ts
+++ ENDABGABE/Server/server.ts
@@ -1,18 +1,26 @@
 import * as Http from "http";
 import * as Url from "url";
+import * as Mongo from "mongodb";
 
 export namespace ENDABGABE_EIA2 {
-    let server: Http.Server = Http.createServer();
 
-    let port: number | string | undefined = process.env.PORT;
-    if (port == undefined)
-        port = 5001;
+    let rocket: Mongo.Collection;
+    let databaseUrl: string = "mongodb+srv://helenareimold:fireworkseditor@cluster1.dyyg0.mongodb.net/FireworksEditor?retryWrites=true&w=majority";
+    startServer();
+    connectToDatabase(databaseUrl);
 
-    console.log("Server starting on port:" + port);
+    function startServer(): void {
+        let server: Http.Server = Http.createServer();
 
-    server.listen(port);
-    server.addListener("request", handleRequest);
+        let port: number | string | undefined = process.env.PORT;
+        if (port == undefined)
+            port = 5001;
 
+        console.log("Server starting on port:" + port);
+        server.listen(port);
+        server.addListener("request", handleRequest);
+
+    }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         _response.setHeader("content-type", "text/html; charset=utf-8");
@@ -25,5 +33,13 @@ export namespace ENDABGABE_EIA2 {
         }
 
         _response.end();
+    }
+
+    async function connectToDatabase(_url: string): Promise<void> {
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        rocket = mongoClient.db("FireworksEditor").collection("Rockets");
+    
     }
 }
